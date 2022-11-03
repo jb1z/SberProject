0b684a209a3b1af2dfe49dd90ff7384eb8ef341c
jbiz <pozolotusiko@mail.ru>
2022-11-03 22:51:28 +0300

    Modified parsing info

---
+++ scr/com/catalog/CityUtils.java
@@ -0,0 +1,79 @@
+package com.catalog;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class CityUtils {
+    /**
+     * Checks if scanner has a line to parse
+     *
+     * @param scanner receives a scanner
+     * @return null if scanner has nothing to parse or a parsed string
+     *
+     */
+    private static String hasNext(Scanner scanner) {
+        String string;
+        if(scanner.hasNext()) {
+            string = scanner.next();
+        } else {
+            string = null;
+        }
+        return string;
+    }
+
+    /**
+     * Uploading data about cities to a list
+     *
+     * @return a list of cities
+     */
+    public static List<City> parse() {
+        List<City> cities = new ArrayList<>();
+        try {
+            String filename = "file/city_ru.csv";
+            Path path = Paths.get(filename);
+            Scanner scanner = new Scanner(path);
+            scanner.useDelimiter(System.getProperty("line.separator"));
+            while(scanner.hasNext()){
+                cities.add(parseToCity(scanner.next()));
+            }
+            scanner.close();
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return cities;
+    }
+
+    /**
+     * Printing the list of cities to console
+     *
+     * @param cities list of cities
+     */
+    public static void print(List<City> cities){
+        cities.forEach(System.out::println);
+    }
+
+    /**
+     * Parsing a string from the file
+     *
+     * @param nextLine receives a string with a data
+     * @return {@link City}
+     */
+    private static City parseToCity(String nextLine){
+        Scanner scanner = new Scanner(nextLine);
+        scanner.useDelimiter(";");
+        scanner.skip("\\d*");
+        String parsedName = hasNext(scanner);
+        String parsedRegion = hasNext(scanner);
+        String parsedDistrict = hasNext(scanner);
+        String parsedPopulation = hasNext(scanner);
+        String parsedFoundation = hasNext(scanner);
+        return new City(parsedName, parsedRegion, parsedDistrict, parsedPopulation, parsedFoundation);
+    }
+}
--- scr/com/catalog/Main.java
+++ scr/com/catalog/Main.java
@@ -1,22 +1,10 @@
 package com.catalog;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Scanner;
+import static com.catalog.CityUtils.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String filename = "file/city_ru.csv";
-        Path path = Paths.get(filename);
-        Scanner scanner = new Scanner(path);
-        ArrayList<City> cities = new ArrayList<>();
-
-        scanner.useDelimiter(System.getProperty("line.separator"));
-        while(scanner.hasNext()){
-            cities.add(City.parseToCity(scanner.next()));
-        }
-        System.out.println(cities);
+        print(parse());
     }
 }
