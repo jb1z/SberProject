4977c26689c480039986a62406ec5cf07598a030
jbiz <pozolotusiko@mail.ru>
2022-11-10 00:20:01 +0300

    Searching and counting by region added

--- scr/com/catalog/Main.java
+++ scr/com/catalog/Main.java
@@ -8,52 +8,27 @@ import static com.catalog.CityUtils.*;
 public class Main {
     public static void main(String[] args) {
         List<City> cities = parse();
-        sortByNameV1(cities); // Сортировка массива по наименованию городов, используя lambda-выражения
-        print(cities); // Вывод данных массива с городами в консоль
-
-        sortByNameV2(cities); // Сортировка массива по наименованию городов, используя java.util.Comparator
-        print(cities); // Вывод данных массива с городами в консоль
-
-        sortByDistrictAndName(cities); // Сортировка массива по федеральным округам и наименваниям городов в них
-        print(cities); // Вывод данных массива с городами в консоль
+        sortByRegion(cities);
+        int count = 0;
+        String region = cities.get(0).getRegion();
+        String anotherRegion;
+        for(City city : cities){
+            anotherRegion = city.getRegion();
+            if(anotherRegion.equals(region)){
+                count++;
+            } else {
+                System.out.println(region + ": " + count);
+                count = 1;
+            }
+            region = city.getRegion();
+        }
     }
-
-    /**
-     * Сортировка массива городов по наименованию в алфавитном порядке по убыванию
-     * без учета регистра, используя lambda-выражения
-     *
-     * @param cities массив городов
-     */
-    private static void sortByNameV1(List<City> cities) {
-        cities.sort((o1, o2) -> o1.getName().compareToIgnoreCase(o2.getName()));
-    }
-
-    /**
-     * Сортировка массива городов по наименованию в алфавитном порядке по убыванию
-     * без учета регистра, используя {@link java.util.Comparator}
-     *
-     * @param cities массив городов
-     */
-    private static void sortByNameV2(List<City> cities) {
+    private static void sortByRegion(List<City> cities) {
         cities.sort(new Comparator<>() {
             @Override
             public int compare(City o1, City o2) {
-                return o1.getName().compareToIgnoreCase(o2.getName());
+                return o1.getRegion().compareToIgnoreCase(o2.getRegion());
             }
         });
-    }
-
-    /**
-     * Сортировка массива городов по федеральному округу и наименованию города внутри каждого федерального округа
-     * в алфавитном порядке по убыванию с учетом регистра
-     *
-     * @param cities массив городов
-     */
-    private static void sortByDistrictAndName(List<City> cities) {
-        cities.sort(Comparator.comparing(City::getDistrict).thenComparing(City::getName));
-    }
-
-    public static void print(List<City> cities) {
-        cities.forEach(System.out::println);
     }
 }
