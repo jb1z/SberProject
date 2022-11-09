2f102e9789ff533732125478936c6b38b6218eb9
jbiz <pozolotusiko@mail.ru>
2022-11-09 23:54:42 +0300

    Right searching

--- scr/com/catalog/Main.java
+++ scr/com/catalog/Main.java
@@ -1,27 +1,65 @@
 package com.catalog;
 
+import java.text.MessageFormat;
+import java.util.Comparator;
 import java.util.List;
 
 import static com.catalog.CityUtils.*;
 
 public class Main {
+    /**
+     * Поиск города с наибольшим количеством жителей путем простого перебора
+     *
+     * @param cities массив городов
+     */
+    private static void findBySimpleBruteForce(List<City> cities) {
+        City[] array = new City[cities.size()];
+        cities.toArray(array);
+        City current = array[0];
+        int index = 0;
+
+        for (int i = 1; i < array.length; i++) {
+            if (Integer.parseInt(array[i].getPopulation()) > Integer.parseInt(current.getPopulation())) {
+                current = array[i];
+                index = i;
+            }
+        }
+        System.out.println(MessageFormat.format("[{0}] = {1}", index, array[index]));
+    }
+
+    /**
+     * Поиск города с наибольшим количеством жителей путем сортировки вставками
+     *
+     * @param cities массив городов
+     */
+    private static void findByInsertionSort(List<City> cities) {
+        City[] array = new City[cities.size()];
+        cities.toArray(array);
+        for (int i = 1; i < array.length; i++) {
+            City current = array[i];
+            int j = i - 1;
+            while (j >= 0 && Integer.parseInt(current.getPopulation()) < Integer.parseInt(array[j].getPopulation())) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = current;
+        }
+        System.out.println(MessageFormat.format("[{0}] = {1}", array.length - 1, array[array.length - 1]));
+    }
+
+    /**
+     * Поиск города с наибольшим количеством жителей с использованием lambda-выражений
+     *
+     * @param cities массив городов
+     */
+    private static void findMaxPopulation(List<City> cities) {
+        System.out.println(cities.stream().max(Comparator.comparing(City::getPopulation)));
+    }
     public static void main(String[] args) {
         List<City> cities = parse();
-        City[] citiesAsArray = cities.toArray(new City[0]);
-        int indexOfMaxPopulation = 0;
-        int maxPopulation = 0;
-        int cityPopulation = 0;
-        int i = 0;
-        for(City city : citiesAsArray) {
-            if(city.getPopulation() != null) {
-                cityPopulation = Integer.parseInt(city.getPopulation());
-            }
-            if(cityPopulation > maxPopulation) {
-                maxPopulation = cityPopulation;
-                indexOfMaxPopulation = i;
-            }
-            i++;
-        }
-        System.out.println("[" + indexOfMaxPopulation + "]: " + maxPopulation);
+        findBySimpleBruteForce(cities);
+        findByInsertionSort(cities);
+        findMaxPopulation(cities);
     }
+
 }
